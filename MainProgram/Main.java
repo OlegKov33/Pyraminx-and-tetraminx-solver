1907b4b6be2a18c79f84ad11084207d768fba545
OlegKov33 <olkova.oleg.kovalchuk@gmail.com>
2025-07-07 19:12:26 +0100

    Updated version

--- MainProgram/Main.java
+++ MainProgram/Main.java
@@ -1,30 +1,29 @@
 class Main{
-    public static void main(String[] args) {
-        
-    //TO COMMENT AND UNCOMMENT SELECT A LINE OF CODE, PRESS AND HOLD CTRL AND PRESS (/)
-    //colour scheme from grubiks to code.
-    //0 - red
-    //1 - blue
-    //2 - yellow
-    //3 - green
-
-
-    int[][] initialStateData = {{2,0,0,0,2,2},{1,1,1,1,1,1},{2,2,3,3,3,2},{0,3,3,3,0,0}};
-
-
-    // Create your own node state with random node generator below.
-    // RandomNodeGenerator nodeGenerator = new RandomNodeGenerator(initialStateData,1);
-    // int[][] outputNodeState = nodeGenerator.getNodeData();
-
-
-    //TO SOLVE UNCOMMENT 2 LINES BELOW
-    Node startNode = new Node("none", "none", initialStateData, 0);
-    Node goalNode = new Node();
+    public static void main(String[] args){
     
+        // use scrambler to get a scramboled initialState that can then be used as initial or goal state
+        Scrambler scrambler = new Scrambler();
+        int[][] initialState = scrambler.scramble(1);
+        int[][] goalState = {{0, 0, 0, 0, 0, 0},{1, 1, 1, 1, 1, 1},{2, 2, 2, 2, 2, 2},{3, 3, 3, 3, 3, 3}};
+        
+        
 
-    //This is code starts the searching algorithm.
-    Problem test = new Problem(startNode, goalNode);
-    test.start();
+        // This code takes unsolved tetraminx and tried to solve it
+
+        // Node initialNode = new Node(initialState, 0, goalState, "none", "none");
+        // Node goalNode = new Node(goalState, 0, goalState, "none", "none");
+
+        // Calculations solver = new Calculations(initialNode, goalNode);
+        // solver.start();
+
+
+
+
+        //This code takes the solved tetraminx and scrambles it
+
+        Node scrambleNode = new Node(goalState, 0, goalState, "none", "none");
+        Node scrambleGoalNode = new Node(initialState, 0, goalState, "none", "none");
+        Calculations scramble = new Calculations(scrambleNode, scrambleGoalNode);
+        scramble.start();
     }
 }
-//Author OlegKov33
--- MainProgram/Node.java
+++ MainProgram/Node.java
@@ -1,87 +1,178 @@
-public class Node {
-    //default values.
-    String nodeName = "none";
-    String nodeParent = "none";
-    int[][] nodeState = new int[4][6];
-    int nodeCost = 0;
+public class Node implements Comparable<Node>{
+    private int[][] state;
+    private int cost;
+    private int[][] goalState;
+    private String name;
+    private String parent;
 
 
-    Node(String givenName, String parent, int[][]givenNodeState, int givenCost){
-        this.nodeName = givenName;
-        this.nodeParent = parent;
-        this.nodeState = givenNodeState;
-        this.nodeCost = givenCost;
+    public Node(){
+        state = new int[][]{{0, 0, 0, 0, 0, 0},{1, 1, 1, 1, 1, 1},{2, 2, 2, 2, 2, 2},{3, 3, 3, 3, 3, 3}};
+        cost = 0;
+        goalState = new int[][]{{0, 0, 0, 0, 0, 0},{1, 1, 1, 1, 1, 1},{2, 2, 2, 2, 2, 2},{3, 3, 3, 3, 3, 3}};
     }
 
 
-    // Empty constructor. Solved tetraminx
-    Node(){
-        int[][] defaultNodeState = {{0,0,0,0,0,0},{1,1,1,1,1,1},{2,2,2,2,2,2},{3,3,3,3,3,3}};
-        nodeState = defaultNodeState;
+    public Node(int[][] inputState, int inputCost,
+                int[][] inputGoalState, String inputName,
+                String inputParent){
+
+        this.state = inputState;
+        this.cost = inputCost;
+        this.goalState = inputGoalState;
+        this.name = inputName;
+        this.parent = inputParent;
     }
-
-
-    //used in (calculations) class to get data of node.
-    public int[] getNodeState(int side){
-        return this.nodeState[side];
-    }
-
-
-    // TO UNCOMMENT OR COMMETN, SELECT A LINE OF CODE, PRESS AND HOLD (LEFT CTRL) AND PRESS (/)
-    // NOTE THAT AT A TIME ONLY (1) @OVERRIDE MUST BE UNCOMMENTED.
     
+    // checks the equality of nodes
     @Override
-    public String toString() {
-        String output = "";
-        output=
-        "Name: \t"+this.nodeName+"\n"+
-        "Parent: "+this.nodeParent+"\n"+
-        "Cost: \t"+this.nodeCost+"\n"+
-        "State: \t";
+    public boolean equals(Object obj) {
+        if(obj.getClass() != Node.class){
+            return false;
+        }
 
-        for(int i = 0; i<this.nodeState.length;i++){
-            for(int j =0;j<this.nodeState[0].length;j++){
-                output+=this.nodeState[i][j];
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 6; j++){
+                if( ( (Node) obj).getNodeSide(i)[j] != state[i][j] ){
+                    return false;
+                }
             }
-            output+="\n\t";
-        }output+="\n";
+        }
+        return true;
+    }
+    
+    // used in similar way as toString. However, its used for naming nodes and in path finding
+    public String printNode(){
+        String output = "";
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 6; j++){
+                output += state[i][j];
+            }
+        }
         return output;
     }
 
 
-    //This does letters - colour scheme from grubiks
-    //0 - red
-    //1 - blue
-    //2 - yellow
-    //3 - green
+    public int[] getNodeSide(int inputSide){
+        return state[inputSide];
+    }
 
+    public int[][] getNodeState(){
+        return state;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+
+    public int[][] getGoalState(){
+        return goalState;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    
+    public String getParent(){
+        return this.parent;
+    }
+
+    // MY COMPARETO version. Does not guarantee an optimal solution.
+    @Override
+    public int compareTo(Node inputNode) {
+        int correctlyAlignedCells = 0;
+        int inputCorrectlyAlignedCells = 0;
+
+        // checks current nodes number of displaced sides.
+        for(byte i = 0 ; i < 3; i++){
+            if( this.state[i][0] == goalState[i][0]){
+                correctlyAlignedCells --; //test
+                if( this.state[ (i - 2) * -1 ][2] == goalState[ (i - 2) * -1 ][2]){
+                    correctlyAlignedCells += 2;
+                }
+            }
+            if( this.state[3][i * 2] == goalState[3][i * 2]){
+                correctlyAlignedCells --; // test
+                if( this.state[i][5] == goalState[i][5]){
+                    correctlyAlignedCells += 2;
+                }
+            }
+        }
+
+
+        // checks the inputNodes number of displaced sides.
+        for(byte i = 0 ; i < 3; i++){
+            if( inputNode.getNodeState()[i][0] == inputNode.getGoalState()[i][0]){
+                inputCorrectlyAlignedCells --; // test
+                if( inputNode.getNodeState()[ (i - 2) * -1 ][2] == inputNode.getGoalState()[ (i - 2) * -1 ][2]){
+                    inputCorrectlyAlignedCells += 2;
+                }
+            }
+            if( inputNode.getNodeState()[3][i * 2] == inputNode.getGoalState()[3][i * 2]){
+                inputCorrectlyAlignedCells --; //test
+                if( state[i][5] == goalState[i][5]){
+                    inputCorrectlyAlignedCells += 2;
+                }
+            }
+        }
+
+        // the more the cost, the less appealing the node should appear
+        correctlyAlignedCells -= (cost ) ; //test
+        inputCorrectlyAlignedCells -= (inputNode.cost ); //test
+    return Integer.compare(inputCorrectlyAlignedCells, correctlyAlignedCells);
+    }
+
+
+
+    // //DEEPSEEKS COMPARETO version. MUST BE USED WITH HEURISTIC V1 or HEURISTIC V2 below!
+    
     // @Override
-    // public String toString(){
-    //     String output = "\n";
-    //     for(int i = 0; i<this.nodeState.length;i++){
-    //         for(int j =0;j<this.nodeState[0].length;j++){
-    //             switch (this.nodeState[i][j]) {
-    //                 case 0:
-    //                 output+="R";
-    //                 break;
-                
-    //                 case 1:
-    //                 output+="B";
-    //                 break;
-
-    //                 case 2:
-    //                 output+="Y";
-    //                 break;
-
-    //                 case 3:
-    //                 output+="G";
-    //                 break;
-    //             }
-    //         }
-    //         output+="\n";
-    //     }
-    //     return output;
+    // public int compareTo(Node o) {
+    //     // Manhattan distance heuristic (sum of all facelet mismatches)
+    //     int thisHeuristic = this.cost + this.calculateHeuristic();
+    //     int otherHeuristic = o.cost + o.calculateHeuristic();
+    //     return Integer.compare(thisHeuristic, otherHeuristic);
     // }
 
+
+    // // DEEPSEEK HEURISTIC V1. After testing, appears to be the only heuristic 
+    // // capable of finding optimal path. Tested on 1 - 10 turns.
+
+    // private int calculateHeuristic() {
+    //     int mismatch = 0;
+    //     for (int i = 0; i < 4; i++) {
+    //         for (int j = 0; j < 6; j++) {
+    //             if (this.state[i][j] != this.goalState[i][j]) {
+    //                 mismatch++;
+    //             }
+    //         }
+    //     }
+    //     return mismatch;
+    // }
+
+
+
+    // // DEEPSEEK HEURISTIC V2. After testing, appears to be inconsistent,
+    // // capable of delivering the most optimal solution with efficient time but also
+    // // unable to solve certain states, resulting in non optimal time and not optimal solution
+
+    // private int calculateHeuristic() {
+    // int totalDistance = 0;
+    // for (int i = 0; i < 4; i++) {
+    //     for (int j = 0; j < 6; j++) {
+    //         int value = this.state[i][j];
+    //         // Find where this value *should* be in the goal state
+    //         for (int goalI = 0; goalI < 4; goalI++) {
+    //             for (int goalJ = 0; goalJ < 6; goalJ++) {
+    //                 if (goalState[goalI][goalJ] == value) {
+    //                     // Add the distance between (i,j) and (goalI, goalJ)
+    //                     totalDistance += Math.abs(i - goalI) + Math.abs(j - goalJ);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // return totalDistance / 6; // Normalize to avoid overcounting
+    // }
 }
-//Author OlegKov33
